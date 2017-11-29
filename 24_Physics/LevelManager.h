b6a43efe62a51e6bbd0757af81c40d26a805336a
Benjamin M <bcm023@gmail.com>
2017-11-29 16:14:50 -0500

    Level editor version 1

---
+++ 24_Physics/LevelManager.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Definitions.h"
+#include <fstream>
+#include <vector>
+
+namespace Simplex
+{
+	class LevelManager
+	{
+		int levelHeight;
+		int levelWidth;
+
+		static LevelManager* m_pInstance;
+
+		//For future milestone. Need to make functions to draw a few new shapes first
+		std::vector<char> fullLevel;
+
+		std::vector<char> loadedLevel;
+		std::vector<char> heightMap;
+		std::vector<char> objectMap;
+
+		std::vector<vector3> blockPositions;
+		
+	public:
+		static LevelManager* GetInstance();
+		static void ReleaseInstance(void);
+		void SetLevel(char* filename);
+		std::vector<vector3> GetBlockPositions();
+	private:
+		// For the next milestone I will expan the level and smooth out all blocks using newly programmed shapes
+		
+		LevelManager(void);
+		LevelManager(LevelManager const& a_pOther);
+		LevelManager& operator=(LevelManager const& a_pOther);
+		~LevelManager(void);
+		void Release(void);
+		void Init(void);
+		
+	};
+
+}
